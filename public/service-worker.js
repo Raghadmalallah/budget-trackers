d47b95b07a272c20de2f1aaca8ae213d8b86c61b
Raghad Malallah <raghadmalallah@gmail.com>
2021-11-18 20:18:22 -0800

    fetch server worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,11 +1,13 @@
 const FILES_TO_CACHE = [
-    './index.html',
-    './css/styles.css',
-    './js/index.js',
-    './js/idb.js',
-    './manifest.json',
-    './icons/icon-512x512.png',
-    './icons/icon-192x192.png'
+  "./index.html",
+  "./css/styles.css",
+  "./js/index.js",
+  "./js/idb.js",
+  "./manifest.json",
+  "./icons/icon-512x512.png",
+  "./icons/icon-192x192.png",
+  "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
+  "https://cdn.jsdelivr.net/npm/chart.js@2.8.0"
 ];
 
 const CACHE_NAME = "static-cache-v1";
@@ -38,47 +40,33 @@ self.addEventListener("activate", (event) => {
   self.clients.claim();
 });
 
-// self.addEventListener("fetch", event => {
-//   // non GET requests are not cached and requests to other origins are not cached
-//   if (
-//     event.request.method !== "GET" ||
-//     !event.request.url.startsWith(self.location.origin)
-//   ) {
-//     event.respondWith(fetch(event.request));
-//     return;
-//   }
+self.addEventListener("fetch", (event) => {
+  if (event.request.url.includes("/api/") && event.request.method === "GET") {
+    event.respondWith(
+      caches
+        .open(DATA_CACHE_NAME)
+        .then((cache) => {
+          return fetch(event.request)
+            .then((response) => {
+              if (response.status === 200) {
+                cache.put(event.request, response.clone());
+              }
 
-//   // handle runtime GET requests for data from /api routes
-//   if (event.request.url.includes("/api/images")) {
-//     // make network request and fallback to cache if network request fails (offline)
-//     event.respondWith(
-//       caches.open(DATA_CACHE_NAME).then(cache => {
-//         return fetch(event.request)
-//           .then(response => {
-//             cache.put(event.request, response.clone());
-//             return response;
-//           })
-//           .catch(() => caches.match(event.request));
-//       })
-//     );
-//     return;
-//   }
+              return response;
+            })
+            .catch(() => {
+              return cache.match(event.request);
+            });
+        })
+        .catch((err) => console.log(err))
+    );
 
-//   // use cache first for all other requests for performance
-//   event.respondWith(
-//     caches.match(event.request).then(cachedResponse => {
-//       if (cachedResponse) {
-//         return cachedResponse;
-//       }
+    return;
+  }
 
-//       // request is not in cache. make network request and cache the response
-//       return caches.open(DATA_CACHE_NAME).then(cache => {
-//         return fetch(event.request).then(response => {
-//           return cache.put(event.request, response.clone()).then(() => {
-//             return response;
-//           });
-//         });
-//       });
-//     })
-//   );
-// });
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
