10df5c9d649e79545961ad733708e209a4f10f65
Raghad Malallah <raghadmalallah@gmail.com>
2021-11-18 20:30:31 -0800

    db.js adding saving record

--- public/db.js
+++ public/db.js
@@ -28,12 +28,16 @@ request.onerror = function(event) {
 function saveRecord(record) {
   // TODO: this function should save a transaction object to indexedDB so that
   // it can be synced with the database when the user goes back online.
+  const transaction = db.transaction("pending", "readwrite");
+  const store = transaction.objectStore("pending");
+  store.add(record);
 }
 
 function checkDatabase() {
   // TODO: this function should check for any saved transactions and post them
   // all to the database. Delete the transactions from IndexedDB if the post
   // request is successful.
+  
 }
 
 // listen for app coming back online
